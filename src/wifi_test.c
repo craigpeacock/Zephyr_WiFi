e64a75874b8b81716d2a84abddc60716916c147a
Craig Peacock <craigp@beyondlogic.org>
2023-01-07 16:19:34 +1030

    Initial release. IPv4 only.

---
+++ src/wifi_test.c
@@ -0,0 +1,145 @@
+/*
+ * Copyright (c) 2023 Craig Peacock.
+ * Copyright (c) 2017 ARM Ltd.
+ * Copyright (c) 2016 Intel Corporation.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <zephyr/kernel.h>
+#include <zephyr/net/net_if.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/net/net_event.h>
+#include <errno.h>
+
+#define SSID "test_ap"
+#define PSK "secret"
+
+static struct net_mgmt_event_callback wifi_cb;
+static struct net_mgmt_event_callback ipv4_cb;
+
+static void handle_wifi_connect_result(struct net_mgmt_event_callback *cb)
+{
+    const struct wifi_status *status = (const struct wifi_status *)cb->info;
+
+    if (status->status)
+    {
+        printk("Connection request failed (%d)\n", status->status);
+    }
+    else
+    {
+        printk("Connected\n");
+    }
+}
+
+static void handle_wifi_disconnect_result(struct net_mgmt_event_callback *cb)
+{
+    const struct wifi_status *status = (const struct wifi_status *)cb->info;
+
+    if (status->status)
+    {
+        printk("Disconnection request (%d)\n", status->status);
+    }
+    else
+    {
+        printk("Disconnected\n");
+    }
+}
+
+static void handle_ipv4_result(struct net_if *iface)
+{
+    int i = 0;
+
+    for (i = 0; i < NET_IF_MAX_IPV4_ADDR; i++) {
+
+        char buf[NET_IPV4_ADDR_LEN];
+
+        if (iface->config.ip.ipv4->unicast[i].addr_type != NET_ADDR_DHCP) {
+            continue;
+        }
+
+        printk("IPv4 address: %s\n",
+                net_addr_ntop(AF_INET,
+                                &iface->config.ip.ipv4->unicast[i].address.in_addr,
+                                buf, sizeof(buf)));
+        printk("Subnet: %s\n",
+                net_addr_ntop(AF_INET,
+                                &iface->config.ip.ipv4->netmask,
+                                buf, sizeof(buf)));
+        printk("Router: %s\n",
+                net_addr_ntop(AF_INET,
+                                &iface->config.ip.ipv4->gw,
+                                buf, sizeof(buf)));
+        }
+}
+
+static void wifi_mgmt_event_handler(struct net_mgmt_event_callback *cb, uint32_t mgmt_event, struct net_if *iface)
+{
+    switch (mgmt_event)
+    {
+
+        case NET_EVENT_WIFI_CONNECT_RESULT:
+            handle_wifi_connect_result(cb);
+            break;
+
+        case NET_EVENT_WIFI_DISCONNECT_RESULT:
+            handle_wifi_disconnect_result(cb);
+            break;
+
+        case NET_EVENT_IPV4_ADDR_ADD:
+            handle_ipv4_result(iface);
+            break;
+
+        default:
+            break;
+    }
+}
+
+void wifi_connect(void)
+{
+    struct net_if *iface = net_if_get_default();
+
+    struct wifi_connect_req_params wifi_params = {0};
+
+    wifi_params.ssid = SSID;
+    wifi_params.psk = PSK;
+    wifi_params.ssid_length = strlen(SSID);
+    wifi_params.psk_length = strlen(PSK);
+    wifi_params.channel = WIFI_CHANNEL_ANY;
+    wifi_params.security = WIFI_SECURITY_TYPE_PSK;
+
+    printk("Connecting to SSID: %s\n", wifi_params.ssid);
+
+    if (net_mgmt(NET_REQUEST_WIFI_CONNECT, iface, &wifi_params, sizeof(struct wifi_connect_req_params)))
+    {
+        printk("WiFi Connection Request Failed\n");
+    }
+}
+
+void wifi_disconnect(void)
+{
+    struct net_if *iface = net_if_get_default();
+
+    if (net_mgmt(NET_REQUEST_WIFI_DISCONNECT, iface, NULL, 0))
+    {
+        printk("WiFi Disconnection Request Failed\n");
+    }
+}
+
+void main(void)
+{
+    net_mgmt_init_event_callback(&wifi_cb, wifi_mgmt_event_handler,
+                                 NET_EVENT_WIFI_CONNECT_RESULT | NET_EVENT_WIFI_DISCONNECT_RESULT);
+
+    net_mgmt_init_event_callback(&ipv4_cb, wifi_mgmt_event_handler, NET_EVENT_IPV4_ADDR_ADD);
+
+    net_mgmt_add_event_callback(&wifi_cb);
+    net_mgmt_add_event_callback(&ipv4_cb);
+
+    wifi_connect();
+
+    // Stay connected for 30 seconds, then disconnect.
+    k_sleep(K_SECONDS(30));    
+
+    wifi_disconnect();
+}
